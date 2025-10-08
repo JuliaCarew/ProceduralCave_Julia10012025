12f39fdb52071f87ed2d2807fc43c6da9d88e1b6
JuliaCarew <crewjulia3@gmail.com>
2025-10-08 10:27:32 -0300

    vid 3

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -21,6 +21,11 @@ public class MapGenerator : MonoBehaviour
         GenerateMap();
     }
 
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            GenerateMap();
+    }
 
     void GenerateMap()
     {
@@ -32,6 +37,9 @@ public class MapGenerator : MonoBehaviour
         {
             SmoothMap();
         }
+
+        MeshGenerator meshGen = GetComponent<MeshGenerator>();
+        meshGen.GenerateMesh(map, 1);
     }
 
     void RandomFillMap()
@@ -59,7 +67,16 @@ public class MapGenerator : MonoBehaviour
     {
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
+                int neighborWallTiles = GetSurroundingWallCount(x ,y);
 
+                if(neighborWallTiles > 4)
+                {
+                    map[x, y] = 1;
+                }
+                else if (neighborWallTiles < 4)
+                {
+                    map[x, y] = 0;
+                }
             }
         }
     }
@@ -70,28 +87,29 @@ public class MapGenerator : MonoBehaviour
         for (int neighborX = gridX - 1; neighborX <= gridX + 1; neighborX++) {
             for (int neighborY = gridY - 1; neighborY <= gridY + 1; neighborY++) {
                 
-                if(neighborX >= 0 && neighborX < width && neighborY >= 0 && neighborY < height)
-                {
-
+                if(neighborX >= 0 && neighborX < width && neighborY >= 0 && neighborY < height) {
+                    if (neighborX != gridX || neighborY != gridY)
+                        wallCount += map[neighborX, neighborY];
                 }
-                if(neighborX != gridX || neighborY != gridY)
+                else
                 {
-                    wallCount += map[neighborX, neighborY];
+                    wallCount++;
                 }
             }
         }
+        return wallCount;
     }
 
-    private void OnDrawGizmos()
-    {
-        if(map != null) {
-            for (int x = 0; x < width; x++) {
-                for (int y = 0; y < height; y++) {
-                    Gizmos.color = (map[x, y] == 1) ? Color.black : Color.white;
-                    Vector3 pos = new Vector3(-width/2 + x + .5f, 0, -height/2 + y + .5f);
-                    Gizmos.DrawCube(pos, Vector3.one);
-                }
-            }
-        }
-    }
+    //private void OnDrawGizmos()
+    //{
+    //    if(map != null) {
+    //        for (int x = 0; x < width; x++) {
+    //            for (int y = 0; y < height; y++) {
+    //                Gizmos.color = (map[x, y] == 1) ? Color.black : Color.white;
+    //                Vector3 pos = new Vector3(-width/2 + x + .5f, 0, -height/2 + y + .5f);
+    //                Gizmos.DrawCube(pos, Vector3.one);
+    //            }
+    //        }
+    //    }
+    //}
 }
